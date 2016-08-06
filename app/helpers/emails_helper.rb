e53c8a5019deb2acd37fb19dc84eb461e42f06df
Sunny Rekhi <rohit.rekhi@gmail.com>
2016-08-05 20:45:23 -0500

    more progress on API call

--- app/helpers/emails_helper.rb
+++ app/helpers/emails_helper.rb
@@ -1,24 +1,20 @@
 require 'google/apis/gmail_v1'
-Gmail = Google::Apis::GmailV1 # Alias the module
+Gmail = Google::Apis::GmailV1 
 service = Gmail::GmailService.new
-
-serice.authorization = authorization
-
-
-
+service.authorization = authorization 
+#when going to Gmail API methods, click on Service class to get the associated methods.
 #user_id = user's email address https://developers.google.com/gmail/api/v1/reference/users/getProfile
 
 
-#Grab the messages for this user
-all_emails = service.list_user_messages()
+date =  Time.now.strftime("%Y/%m/%d")  # THIS IS HOW YOU GET TODAY DATE IN YY/MM/DD
 
 
-
-
-
-
-private
-
-def get_user_id(current_user)
-
+user_emails = service.list_user_messages(current_user.email, q:"#{date}") #Q param == search parameter
+for u in users_email
+  temp_email = service.get_user_message(current_user.email, u.id) 
+  #add this temp value to an array
 end
+
+#cycle through array and use regex to grab the body, scan body and regex for article then save all pulled articles in backend.
+  
+
